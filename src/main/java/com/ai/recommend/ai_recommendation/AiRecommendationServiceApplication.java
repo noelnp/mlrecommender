ba377ad31bc7a1fbbf0978502ecedcb7d9c69599
noelnp <noel.peltomaki@gmail.com>
2025-01-17 11:24:26 +0100

    Initial commit: Set up String Boot project

---
+++ src/main/java/com/ai/recommend/ai_recommendation/AiRecommendationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ai.recommend.ai_recommendation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiRecommendationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiRecommendationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ai/recommend/ai_recommendation/AiRecommendationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai.recommend.ai_recommendation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiRecommendationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
